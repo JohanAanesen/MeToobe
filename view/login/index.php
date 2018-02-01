c25001bdeab3049a41f6cfe1ef9d454f53d38c38
Johan Aanesen <johanaan@stud.ntnu.no>
2018-02-02 00:16:44 +0100

    small changes to links around :) (i used some wrongly)

--- view/login/index.php
+++ view/login/index.php
@@ -1,11 +1,27 @@
 <?php
+session_start();
+
 $ROOT = $_SERVER['DOCUMENT_ROOT'];
 
 require_once "$ROOT/vendor/autoload.php";
+require_once "$ROOT/classes/DB.php";
+require_once "$ROOT/classes/user.php";
 
 $loader = new Twig_Loader_Filesystem("$ROOT/twig");
 $twig = new Twig_Environment($loader, array(
     // 'cache' => './cache', /* Only enable cache when everything works correctly */
 ));
 
-echo $twig->render('login.html');
+$db = DB::getDBConnection();
+
+$user = new User($db);
+
+$data = [];
+
+if ($user->loggedIn()) {
+    header('Location: /');
+}else{
+    echo $twig->render('login.html');
+}
+
+
