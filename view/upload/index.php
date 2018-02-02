3fc884ecb97778df8e93584b63a50a731054fb26
Johan Aanesen <johanaan@stud.ntnu.no>
2018-02-02 14:41:25 +0100

    small template changes to change logged in behavior. Also .htaccess edit

--- view/upload/index.php
+++ view/upload/index.php
@@ -1,13 +1,34 @@
 <?php
+session_start();
 
 $ROOT = $_SERVER['DOCUMENT_ROOT'];
 
 require_once "$ROOT/vendor/autoload.php";
+require_once "$ROOT/classes/DB.php";
+require_once "$ROOT/classes/user.php";
 
 $loader = new Twig_Loader_Filesystem("$ROOT/twig");
 $twig = new Twig_Environment($loader, array(
-    // 'cache' => './cache', /* Only enable cache when everything works correctly */
+// 'cache' => './cache', /* Only enable cache when everything works correctly */
 ));
 
+$db = DB::getDBConnection();
 
-echo $twig->render("upload.html");
+$user = new User($db);
+
+$data = [];
+
+if ($user->loggedIn()){
+    echo $twig->render('upload.html', array(
+        'title' => 'home',
+        'data' => $data,
+        'loggedin' => 'yes',
+        'user' => $user->userData,
+    ));
+}else{
+    echo $twig->render('upload.html', array(
+        'title' => 'home',
+        'data' => $data,
+        'loggedin' => 'no',
+    ));
+}
