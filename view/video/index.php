68a52a69e1c7dd41122c5ced0fa9744579163706
Johan Aanesen <johanaan@stud.ntnu.no>
2018-02-04 00:33:42 +0100

    small id-failsafe for the vote buttons

--- view/video/index.php
+++ view/video/index.php
@@ -22,9 +22,9 @@ $likes = 0;
 $dislikes = 0;
 $hasLiked = false;
 
-if (isset($_POST['upvote'])){
+if (isset($_POST['upvote']) && isset($_POST['id'])){
     Video::videoVote($db, $_GET['id'], $_SESSION['userid'], true);
-}else if(isset($_POST['downvote'])){
+}else if(isset($_POST['downvote']) && isset($_POST['id'])){
     Video::videoVote($db, $_GET['id'], $_SESSION['userid'], false);
 }
 
