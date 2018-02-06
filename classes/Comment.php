3d61f12a594bb94d6900feaaed91f4e75571fc4e
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-06 11:24:47 +0100

    Comment.php - add() and delete()

--- classes/Comment.php
+++ classes/Comment.php
@@ -1,5 +1,24 @@
 <?php
 
 class Comment {
-    
+    public static function add($db, $userid, $videoid, $comment) {
+        $commentid = uniqid();
+
+        $sql = "INSERT INTO comment (commentid, user, video, comment) VALUES (?, ?, ?, ?)";
+        $stmt = $db->prepare($sql);
+        $param = array($commentid, $userid, $videoid, $comment);
+        $stmt->execute($param);
+
+        if ($stmt->rowCount() !== 1) {  
+            return 0;
+        }
+        return $commentid;
+    }
+
+    public static function delete($db, $commentid) {
+        $sql = "DELETE FROM comments WHERE commentid=?";
+        $sth = $db->prepare($sql);
+        $param = array($commentid);
+        $sth->execute($commentid);
+    }
 }
