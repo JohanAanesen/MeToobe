6054cd967cddec08f956978e9f7a74ff545350c7
Johan Aanesen <johanaan@stud.ntnu.no>
2018-02-06 13:18:03 +0100

    small bugfix for guests, also removed comment box for guests.

--- view/video/index.php
+++ view/video/index.php
@@ -2,7 +2,6 @@
 session_start();
 
 $ROOT    = $_SERVER['DOCUMENT_ROOT'];
-$userid  = $_SESSION['userid'];
 $videoid = $_GET['id'];
 
 //if videoid is not set, return user to frontpage.
@@ -19,6 +18,10 @@ require_once "$ROOT/classes/Comment.php";
 
 $db = DB::getDBConnection();
 $user = new User($db);
+$userid = "";
+if($user->loggedIn()) {
+    $userid = $_SESSION['userid'];
+}
 $data = [];
 
 $videoData = null;
@@ -27,6 +30,7 @@ $likes = 0;
 $dislikes = 0;
 $hasLiked = false;
 
+// Voting, only if user is logged in and corresponding post requests are made.
 if($user->loggedIn()){
     if (isset($_POST['upvote'])){
         Video::videoVote($db, $videoid, $userid, true);
@@ -44,6 +48,7 @@ Video::viewCountPlus($db, $videoid);
 $videoData = Video::findVideo($db, $videoid);
 $videoLikes = Video::findLikes($db, $videoid);
 
+// Counts up likes and dislikes from db
 if(isset($videoLikes)){
     foreach ($videoLikes as $like){
         if ($like['vote']==true){
