bb759ccfbd7df1c3a3aab15ccb5f5eb59886e99b
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-10 15:12:48 +0100

    created playlist files

---
+++ php/playlistCreate.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+require_once "$ROOT/php/requirelogin.php";
---
+++ php/playlistDelete.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+require_once "$ROOT/php/requirelogin.php";
+
