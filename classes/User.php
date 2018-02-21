d8cab6bf83d02ecb280aebb46867f5acb6684078
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-21 17:40:35 +0100

    User.php - Moving all session managment inside this class. - Also got the create() and login() functions more robust

--- classes/User.php
+++ classes/User.php
@@ -2,6 +2,10 @@
 
 
 class User {
+
+    private static $KEY_SESSION_USERID = 'userid';
+    private static $KEY_SESSION_USERTYPE = 'usertype';
+
     /**
      * @function create
      *  @brief  Create a valid new user in the database.
@@ -14,16 +18,29 @@ class User {
      *  @return userid | 0
      */
     static function create($db, $fullname, $email, $password, $wannebeTeacher) {
-        
+
+        // Is email already registered?
+        $query = "SELECT * FROM User WHERE email = ?";
+        $param = array($email);
+        $stmt = $db->prepare($query);
+        $stmt->execute($param);
+    
+        // @error A user with that email already registered.
+        if ($stmt->fetchColumn() > 0) {
+            return 0;
+        }
+
         $query = "INSERT INTO user (id, fullname, email, password, usertype, wannabe) VALUES (?, ?, ?, ?, ?, ?)";
-        $param = array(uniqid(), $fullname, $email, md5($password), "student", $wannabe);
+        $userid = uniqid();
+        $param = array($userid, $fullname, $email, md5($password), "student", $wannebeTeacher);
         $stmt = $db->prepare($query);
         $stmt->execute($param);
 
+        // @error There was an error when inserting the user
         if ($stmt->rowCount() !== 1) {
             return 0;
         }
-        return $db->lastInsertId();        
+        return $userid;      
     }
 
     /**
@@ -31,6 +48,7 @@ class User {
       *  @param  db:       PDOConnection
       *  @param  email:    string - Example: jonas.test@gmail.com
       *  @param  password: string - Example: "my secret awesome password is magic"
+      *  @global $_SESSION
       *  @return userid | 0
       */
     static function login($db, $email, $password) {
@@ -45,44 +63,65 @@ class User {
         }
 
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $_SESSION['userid'] = $row['id'];
 
-        return $_SESSION['userid'];
+        User::requireSession();
+        $_SESSION[User::$KEY_SESSION_USERID] = $row['id'];
+        $_SESSION[User::$KEY_SESSION_USERTYPE] = $row['usertype'];
+
+        return $_SESSION[User::$KEY_SESSION_USERID];
     }
     
     /**
       * @function logout
       * @brief Modified the $_SESSION to in effect log out the user.
+      * @global $_SESSION
       */
     static function logout() {
-        unset($_SESSION['userid']);
+        User::requireSession();
+        
+        unset($_SESSION[User::$KEY_SESSION_USERID]);
+        unset($_SESSION[User::$KEY_SESSION_USERTYPE]);
     }
 
     /**
       * @function isLoggedIn
-      * @param userid: string
+      * @global $_SESSION
       * @return userid | 0
       */
-    static function isLoggedIn($userid) {
-        if( !isset($_SESSION['userid']) ) {
+    static function isLoggedIn() {
+        User::requireSession();
+        
+        if( !isset($_SESSION[User::$KEY_SESSION_USERID]) ) {
             return 0;
         }
-        return $_SESSION['userid'];
+        return $_SESSION[User::$KEY_SESSION_USERID];
     }
 
     /** 
       * @function isLoggedInOrRedirectTo
-      * @param userid: string
       * @param redirectURL: string - URL to redirect to if there is an error
+      * @global $_SESSION
       * @return  userid | redirect to specified redirectURL
       */
-    static function isLoggedInOrRedirectTo($userid, $redirectURL) {
-    
-        if( !isset($_SESSION['userid']) ) {
+    static function isLoggedInOrRedirectTo($redirectURL) {
+        User::requireSession();
+        
+        if( !isset($_SESSION[User::$KEY_SESSION_USERID]) ) {
             header("Location: " . $redirectURL);
             exit();
         }
-        return $_SESSION['userid'];
+        return $_SESSION[User::$KEY_SESSION_USERID];
+    }
+
+    /**
+     * @global $_SESSION
+     */
+    static function isAdmin() {
+        User::requireSession();        
+
+        if ( !isset($_SESSION[User::$KEY_SESSION_USERTYPE]) || $_SESSION[User::$KEY_SESSION_USERTYPE] !== 'admin' )
+            return 0;
+        return $_SESSION[User::$KEY_SESSION_USERID];
     }
 
     /** 
@@ -126,37 +165,40 @@ class User {
      * @requires admin rights 
      * @brief getWannabe grabs all wannabe's from the DB
      * @param db: PDOConnection
-     * @param usertype: enum['admin', 'teacher', 'student']
      * @return array of wannabeTeachers | null
      */
-    static public function getWannabeTeachers($db, $usertype){
-        if($usertype !== 'admin') {
-            return 0;
+    static public function getWannabeTeachers($db){
+        // @error - user is not admin
+        if ( !User::isAdmin() ) {
+            return null;
         }
-
         //SQL Injection SAFE query method:
         $query = "SELECT * FROM user WHERE wannabe = (?)";
         $param = array(true);
         $stmt = $db->prepare($query);
         $stmt->execute($param);
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC));
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     /**
      * @function getUser
      * @param db: PDOConnection
-     * @param $email
-     * @param $password
      * @return user | null
      */
-    static public function getUser($db, $email, $password) {
+    static public function getUser($db, $userid) {
         //SQL Injection SAFE query method:
-        $query = "SELECT * FROM user WHERE email = (?) AND password = (?)";
-        $param = array($email, $password);
+        $query = "SELECT * FROM user WHERE id = (?)";
+        $param = array($userid);
         $stmt = $db->prepare($query);
         $stmt->execute($param);
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    static private function requireSession() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
     }
 }
