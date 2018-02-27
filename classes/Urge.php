d63b480a3010cf79d395a50b5c43c5fcec929e31
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-02-27 15:38:37 +0100

    Urge.php - new requireFileParameter() and scaleThumbnail()

--- classes/Urge.php
+++ classes/Urge.php
@@ -87,4 +87,84 @@ class Urge {
         setcookie('email', null, -1, '/');
       }
     }
+
+    public static function requireFileParameter($fileParam) {
+      if ( !isset($_FILES[$fileParam]) ) {
+          Urge::gotoError(400, 'Bad request, missing parameter' . $fileParam);
+      }   
+      $tmp_filepath = $_FILES[$fileParam]['tmp_name'];
+      $errorCode = $_FILES[$fileParam]['error'];
+
+      if (!is_uploaded_file($tmp_filepath)) {
+          $msg = Urge::getFileuploadErrormessage($errorCode);
+          Urge::gotoError(400, "Bad request on is_uploaded_file, msg: ". $msg);
+      }
+
+      return $_FILES[$fileParam];
+    }
+
+     /**
+      * @note stolen from okolloen examples
+      */ 
+    public static function scaleThumbnail($_img) {
+
+        $new_width = 150;
+        $new_height = 150;
+        $img = imagecreatefromstring($_img);
+
+        $old_x = imageSX($img);
+        $old_y = imageSY($img);
+
+        if($old_x > $old_y) {                     // Image is landscape mode
+            $thumb_w = $new_width;
+            $thumb_h = $old_y*($new_height/$old_x);
+        } else if($old_x < $old_y) {              // Image is portrait mode
+            $thumb_w = $old_x*($new_width/$old_y);
+            $thumb_h = $new_height;
+        } if($old_x == $old_y) {                  // Image is square
+            $thumb_w = $new_width;
+            $thumb_h = $new_height;
+        }
+
+        if ($thumb_w>$old_x) {                    // Don't scale images up
+            $thumb_w = $old_x;
+            $thumb_h = $old_y;
+        }
+
+        $dst_img = ImageCreateTrueColor($thumb_w,$thumb_h);
+        imagecopyresampled($dst_img,$img,0,0,0,0,$thumb_w,$thumb_h,$old_x,$old_y);
+
+        ob_start();                         // flush/start buffer
+        imagepng($dst_img, NULL,9);          // Write image to buffer
+        $scaledImage = ob_get_contents();   // Get contents of buffer
+        ob_end_clean();                     // Clear buffer
+        return $scaledImage;
+    }
+
+    // Function for upload errors
+    public static function getFileuploadErrormessage($errorNumber){
+      // Source: http://php.net/manual/en/function.is-uploaded-file.php
+      // More source: http://php.net/manual/en/features.file-upload.errors.php
+      switch($errorNumber){
+        case UPLOAD_ERR_OK: // This should not show up, but is here just in case
+          return "There is no error, the file uploaded with success."; break;
+        case UPLOAD_ERR_INI_SIZE:
+          return "The uploaded file exceeds the upload_max_filesize directive in php.ini."; break;
+        case UPLOAD_ERR_FORM_SIZE:
+          return "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form."; break;
+        case UPLOAD_ERR_PARTIAL:
+          return "The uploaded file was only partially uploaded."; break;
+        case UPLOAD_ERR_NO_FILE:
+          return "No file was uploaded."; break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+          return "Missing a temporary folder."; break;
+        case UPLOAD_ERR_CANT_WRITE:
+          return "Failed to write file to disk."; break;
+        case UPLOAD_ERR_EXTENSION:
+          return "A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop;
+          examining the list of loaded extensions with phpinfo() may help."; break;
+        default:
+          return "There was a problem with your upload."; break;
+        }
+    }
 }
