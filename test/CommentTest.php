caf2714c2e2d8371ce68294af34e806f28407cba
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-02-27 01:19:42 +0100

    CommentTest.php - fleshed out get/upate/delete tests

--- test/CommentTest.php
+++ test/CommentTest.php
@@ -2,35 +2,90 @@
 
 require_once "./vendor/autoload.php";
 require_once "./classes/DB.php";
+
+require_once "./classes/User.php";
+require_once "./classes/Video.php";
 require_once "./classes/Comment.php";
 
 use PHPUnit\Framework\TestCase;
 
 final class CommentTest extends TestCase {
 
-    private $videoid = "1337";
-    private $userid  = "8888";
+    private $db;
+
+    private $userfullname = "TESTING-TESTING";
+    private $useremail    = "test@test.com";
+    private $userpassword = "testpassword";
+    private $userid       = '';
+
+    private $videoid = '';
+    private $video = array(
+        'name'       => 'testvideo1', 
+        'course'     => 'TDD', 
+        'topic'      => 'Testing', 
+        'description'=> 'Be a better tester!');
+
+    private $commentidSetup;
+    private $commentFromSetup = "Setup test comment TEST TEST TEST !!!!1";
+    private $commentidAddDelete;
+
 
     public function setup() {
-        echo "\nSetting up test environment\n";
-        ob_flush();
+        $this->db     = DB::getDBConnection();
+        $this->userid = User::create(
+            $this->db,
+            $this->userfullname,
+            $this->useremail,
+            $this->userpassword,
+            false);
+        $this->assertNotEquals($this->userid, 0);
+ 
+        $this->videoid = Video::add(
+            $this->db,
+            $this->userid,
+            $this->video['name'],
+            $this->video['course'],
+            $this->video['topic'],
+            $this->video['description']);
+        $this->assertNotEquals(0, $this->videoid);
+
+        $this->commentidSetup = Comment::add($this->db, $this->userid, $this->videoid, $this->commentFromSetup);
+        $this->assertNotEquals(0, $this->commentidSetup);
     }
 
     public function tearDown() {
-        echo "\nTearing down test environment\n";
-        ob_flush();        
+        Comment::delete($this->db, $this->commentidSetup);
+        User::delete($this->db, $this->userid);
+    }  
+
+
+    public function testGetComment() {
+        $comment = Comment::get($this->db, $this->videoid);
+        $this->assertNotEmpty($comment);
+        
+        $comment = $comment[0];
+        $this->assertNotEmpty($comment);
+        $this->assertArrayHasKey('id', $comment);
+        $this->assertArrayHasKey('userid', $comment);
+        $this->assertArrayHasKey('videoid', $comment);
+        $this->assertArrayHasKey('comment', $comment);
+        $this->assertArrayHasKey('time', $comment);
+
+        $this->assertEquals($this->commentFromSetup, $comment['comment']);
     }
 
-    public function testAddCommentWithCorrectID() {
-        $db = DB::getDBConnection();
-        $result = Comment::add($db, $userid, $videoid, "TEST TEST TEST TEST !!!!1");
-       
-        $this->assertNotEquals(0, $result);
+    public function testAddComment() {
+        $this->commentidAddDelete = Comment::add($this->db, $this->userid, $this->videoid, "Test adding comment TEST TEST TEST!");
+        $this->assertNotEquals(0, $this->commentidAddDelete);
     }
 
     /**
      * @depends testAddComment
      */
-    public function testDeleteCommentWithCorrectID() {       
+    public function testDeleteComment() {       
+        $this->commentidAddDelete = Comment::add($this->db, $this->userid, $this->videoid, "Test adding comment TEST TEST TEST!");
+
+        $deleted = Comment::delete($this->db, $this->commentidAddDelete);
+        $this->assertNotEquals($deleted, false);
     }
 }
