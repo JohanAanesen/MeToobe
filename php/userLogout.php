e07e866d8220713e97292e35ebb58218ece4bb57
Jonas Johan Solsvik <jonas.solsvik@gmail.com>
2018-02-27 01:22:18 +0100

    userLogout.php - added missing require_once

--- php/userLogout.php
+++ php/userLogout.php
@@ -1,7 +1,10 @@
 <?php
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+require_once "$ROOT/classes/Urge.php";
 
 session_start();
 session_destroy();
+
 
 if(isset($_COOKIE['email'])){
   unset($_COOKIE['email']);
