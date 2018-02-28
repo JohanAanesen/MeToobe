341f462356ccd2cdb15ab64d6fd0d5cf0e5b9cca
Johan Aanesen <johanaan@stud.ntnu.no>
2018-02-28 19:38:55 +0100

    failsafes/optimized code

--- php/playlistSwap.php
+++ php/playlistSwap.php
@@ -12,6 +12,8 @@ list($playlistid, $videoid, $videoRank, $swap) = Urge::requireParameterArray(
 
 if($swap == 'up' && $videoRank > 0){
     $otherVideoId = Playlist::getVideoIdByRankPlaylist($db, $videoRank-1, $playlistid);
+    if($videoid == $otherVideoId){header("Location: /playlist?id=".$playlistid);}
+
     if(Playlist::swapVideoRank($db, $playlistid, $videoid, $otherVideoId, $videoRank, $videoRank-1)){
         header("Location: /playlist?id=".$playlistid);
     }else{
@@ -19,11 +21,13 @@ if($swap == 'up' && $videoRank > 0){
     }
 }elseif ($swap == 'down' && $videoRank < (Playlist::getPlaylistLength($db, $playlistid)-1)){
     $otherVideoId = Playlist::getVideoIdByRankPlaylist($db, $videoRank+1, $playlistid);
+    if($videoid == $otherVideoId){header("Location: /playlist?id=".$playlistid);}
+    
     if(Playlist::swapVideoRank($db, $playlistid, $videoid, $otherVideoId, $videoRank, $videoRank+1)){
         header("Location: /playlist?id=".$playlistid);
     }else{
         Urge::gotoError(500, "something went to shit.");
     }
 }else{
-    Urge::gotoError(400, 'bad request.. lø');
+    header("Location: /playlist?id=".$playlistid);
 }
